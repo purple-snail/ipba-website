36e6cc6c5ab2c50fa24ff4c4b864f7933f32c141
Richan-Oreta <richanmoreta@gmail.com>
2022-10-20 08:06:02 +0800

    polished the website

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('animate__animated', 'animate__fadeInRightBig')
-            entry.target.classList.remove('group-member')
+            entry.target.classList.add(
+                "animate__animated",
+                "animate__fadeInRightBig"
+            );
         }
-    })
-})
+    });
+});
 
-const groupMembers = document.querySelectorAll('.group-member')
-let ms = 0
+const groupMembers = document.querySelectorAll(".group-member");
+let ms = 0;
 
 groupMembers.forEach((el) => {
-    el.style.setProperty('animation-delay', ms + 'ms')
-    observer.observe(el)
+    el.style.setProperty("animation-delay", ms + "ms");
+    observer.observe(el);
+    ms = ms + 100;
+});
 
-    ms = ms + 100
-})
+window.addEventListener("scroll", (e) => {
+    const nav = document.querySelector(".navbar");
+    if (window.pageYOffset > 0) {
+        nav.classList.add("add-shadow");
+    } else {
+        nav.classList.remove("add-shadow");
+    }
+});
