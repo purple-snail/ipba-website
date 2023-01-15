7ec47e859bdab1ec5e9f845a3969417ebd5ae68d
Richan-Oreta <richanmoreta@gmail.com>
2023-01-15 20:55:37 +0800

    added back arrow to website selection menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const switchMenu = document.getElementById('switch-menu')
+const backArrow = document.getElementById('back-arrow')
 let timeline = gsap.timeline()
 
 switchMenu.onclick = () => {
@@ -15,8 +16,28 @@ switchMenu.onclick = () => {
     })
 
     timeline.to('#back-arrow', {
-        x: 46,
+        opacity: 1,
+        zIndex: 1000,
         duration: 0.5
     })
 }
 
+backArrow.onclick = () => {
+    timeline.to('#website-menu', {
+        opacity: 0,
+        zIndex: 0,
+        duration: 0.5
+    })
+
+    timeline.to('#back-arrow', {
+        opacity: 0,
+        zIndex: 0,
+        duration: 0.5
+    })
+
+    timeline.to('#quarter-menu', {
+        opacity: 1,
+        zIndex: 999,
+        duration: 0.5
+    })
+}
